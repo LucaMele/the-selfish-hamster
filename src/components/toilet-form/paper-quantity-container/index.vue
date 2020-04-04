075ccd8d10ba7087e29b26457cac50b77f7de47c
Cris Vilares <cristofo.vilares@gmail.com>
2020-04-04 20:47:56 +0200

    startet implementation paper-quantity

---
+++ src/components/toilet-form/paper-quantity-container/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <article>
+    <div class="hamster__paper-quantity-container">
+      <div class="back-wrapper">
+        <toilet-back @onClickCallback="onBackCallback"></toilet-back>
+      </div>
+      <div class="paper-wrapper">
+      </div>
+      <div class="next-wrapper">
+        <toilet-next @onClickCallback="onNextCallback"></toilet-next>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+import ToiletBack from './../back';
+import ToiletNext from './../next';
+
+export default {
+  name: 'paper-quantity-container',
+  components: {
+    ToiletBack,
+    ToiletNext,
+  },
+  methods: {
+    onNextCallback() {
+      this.$emit('callbackNext', this.sliderValue);
+    },
+    onBackCallback() {
+      this.$emit('callbackBack');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  @import "./index.scss";
+</style>
