36ee77cd17b9ae9a261f8b1940e8cf222c89b69f
Sven Frei <frei-sven@bluewin.ch>
2020-04-04 17:43:15 +0200

    Added styling for the next and back button.

--- src/components/toilet-form/next/index.vue
+++ src/components/toilet-form/next/index.vue
@@ -1,8 +1,11 @@
 <template>
   <article class="hamster__toilet-next">
-    <div class="lower-bg">
-      <button class="toilet-next-button" @click="onClick">
-        <p>{{$t("pages.general.button.next")}}</p>
+    <div class="toilet-next__container">
+      <button @click="onClick">
+        <p class="toilet-next">{{$t("pages.general.button.next")}}</p>
+        <div class="arrow-right">
+          <img src="./../../../assets/next-arrow.svg"/>
+        </div>
       </button>
     </div>
   </article>
